5c1ae756cf8ae8727841390b10303f7e728d10ac
oncomouse <oncomouse@gmail.com>
2020-08-13 14:57:37 -0500

    Working

--- Formula/vim-language-server.rb
+++ Formula/vim-language-server.rb
@@ -7,7 +7,7 @@ class VimLanguageServer < Formula
   desc 'vim language server'
   homepage 'https://github.com/iamcco/vim-language-server#readme'
   url 'https://registry.npmjs.org/vim-language-server/-/vim-language-server-2.1.0.tgz'
-  sha256 '262a770d25ce82d48548b0a423c8114ead6a30d327d09ad24b3950c1386e99ef'
+  sha256 '2323f6ddad799d569a0e1ec28466bef0779bb8788b5329a0c453d1a3cf43cfd3'
 
   depends_on 'node'
 
